bf425d77bed7e3a0a541a5d4875f4758ce08a6bb
Lars Bendik Bjella Ramstad <brlb022025@gmail.com>
2025-04-07 14:50:18 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Create_methods_that_return_values___complete_the_challenge_to_add_methods_to_make_the_game_playable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        Console.WriteLine("Would you like to play? (Y/N)");
+        if (ShouldPlay())
+        {
+            PlayGame();
+        }
+
+        void PlayGame()
+        {
+            var play = true;
+
+            while (play)
+            {
+                var target;
+                var roll;
+
+                Console.WriteLine($"Roll a number greater than {target} to win!");
+                Console.WriteLine($"You rolled a {roll}");
+                Console.WriteLine(WinOrLose());
+                Console.WriteLine("\nPlay again? (Y/N)");
+
+                play = ShouldPlay();
+            }
+        }
+
+        bool ShouldPlay()
+        {
+            return true;
+        }
+
+        bool WinOrLose()
+        {
+            return true;
+        }
+    }
+}
